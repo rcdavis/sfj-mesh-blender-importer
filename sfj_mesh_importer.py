a09ece98092fa71cc61b9baea4285ee5dae7e2c6
Ren Davis <rcdavisgd@gmail.com>
2025-08-20 06:17:53 -0400

    Added parsing of indices

--- sfj_mesh_importer.py
+++ sfj_mesh_importer.py
@@ -5,8 +5,8 @@ from typing import List, Tuple
 
 @dataclass
 class VertInfluences:
-    influences: Tuple[int, int, int, int]
-    weights: Tuple[float, float, float, float]
+    influences: List[int]
+    weights: List[float]
 
 @dataclass
 class AnimVertex:
@@ -56,10 +56,8 @@ class MESH_OT_ImportSFJ(bpy.types.Operator):
 
         with open(filepath, "rb") as file:
             numTextures = struct.unpack('<I', file.read(4))[0]
-            self.report({'INFO'}, f"The number of textures is: {numTextures}")
 
             numVerts = struct.unpack('<I', file.read(4))[0]
-            self.report({'INFO'}, f"The number of verts is: {numVerts}")
 
             for i in range(numVerts):
                 posx, posy, posz = struct.unpack("<fff", file.read(12))
@@ -67,12 +65,14 @@ class MESH_OT_ImportSFJ(bpy.types.Operator):
                 tanx, tany, tanz = struct.unpack("<fff", file.read(12))
                 texu, texv = struct.unpack("<ff", file.read(8))
 
-                boneInfluences: List[int] = []
-                boneWeights: List[float] = []
+                numInfluences = struct.unpack('<I', file.read(4))[0]
 
-                for curInfluence in range(4):
-                    boneInfluences.append(struct.unpack("<I", file.read(4))[0])
-                    boneWeights.append(struct.unpack("<f", file.read(4))[0])
+                boneInfluences = [0] * numInfluences
+                boneWeights = [0.0] * numInfluences
+                for curInfluence in range(numInfluences):
+                    influence, weight = struct.unpack("<If", file.read(8))
+                    boneInfluences[curInfluence] = influence
+                    boneWeights[curInfluence] = weight
 
                 vertices.append(AnimVertex(
                     pos=(posx, posy, posz),
@@ -80,10 +80,14 @@ class MESH_OT_ImportSFJ(bpy.types.Operator):
                     tan=(tanx, tany, tanz),
                     texCoords=(texu, texv),
                     influences=VertInfluences(
-                        influences=(boneInfluences[0], boneInfluences[1], boneInfluences[2], boneInfluences[3]),
-                        weights=(boneWeights[0], boneWeights[1], boneWeights[2], boneWeights[3])
+                        influences=boneInfluences,
+                        weights=boneWeights
                     )
                 ))
+
+            numFaces = struct.unpack('<I', file.read(4))[0]
+            indices = list(struct.unpack(f"<{numFaces * 3}I", file.read(4 * numFaces * 3)))
+            faces = [indices[i:i+3] for i in range(0, len(indices), 3)]
 
 classes = [MeshImportProperties, MESH_PT_SFJImporter, MESH_OT_ImportSFJ]
 
